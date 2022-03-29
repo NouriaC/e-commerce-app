baef8b2e45e50606c45e97cf5956b66181aedb54
nouria chaouy <nouria.chaouy@gmail.com>
2022-03-29 18:33:10 +0100

    finished cart and working on user functionalities

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
-import styled from 'styled-components'
-import { Home, SingleProduct, Cart, Checkout, Error, About, Products, Private } from './pages'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  Private,
+} from "./pages";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Sidebar/>
+      <Navbar />
+      <Sidebar />
       <Routes>
-       <Route path="/" element={<Home />}/>
-       <Route path="/about" element={<About />}/>
-       <Route path="/cart" element={<Cart />}/>
-       <Route path="/products" element={<Products />}/>
-       <Route path="/products/:id" element={<SingleProduct />}/>
-       <Route path="/checkout" element={<Checkout />}/>
-       <Route path="/error" element={<Error />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route
+          path="/checkout"
+          element={
+            <Private>
+              <Checkout />
+            </Private>
+          }
+        />
+        <Route path="/error" element={<Error />} />
       </Routes>
-      <Footer/>
-     
+      <Footer />
     </Router>
   );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { ProductsProvider } from './context/products_context'
-import { FilterProvider } from './context/filter_context'
-import { CartProvider } from './context/cart_context'
-import { UserProvider } from './context/user_context'
-import { Auth0Provider } from '@auth0/auth0-react'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+// domain: dev-v5969ra4.eu.auth0.com
+// clientID: kdqjLfOfwOJRb74WjbDqyuPX2lTdHXoM
 
 ReactDOM.render(
-<ProductsProvider>
-   <FilterProvider>
-      <CartProvider>
-         <App /> 
-      </CartProvider>
-   </FilterProvider>
-</ProductsProvider>
-, document.getElementById('root'))
+  <Auth0Provider
+    domain="dev-v5969ra4.eu.auth0.com"
+    clientId="kdqjLfOfwOJRb74WjbDqyuPX2lTdHXoM"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
+  document.getElementById("root")
+);
